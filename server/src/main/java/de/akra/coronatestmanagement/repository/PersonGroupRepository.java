713a5f3e864c79e41d2d9060a140aae25f1f0f1e
Marcus Riemer <marcus.riemer@akra.de>
2022-01-28 11:42:32 +0100

    Basic integration of compiled Angular client into Spring Boot jar

--- server/src/main/java/de/akra/coronatestmanagement/repository/PersonGroupRepository.java
+++ server/src/main/java/de/akra/coronatestmanagement/repository/PersonGroupRepository.java
@@ -1,21 +1,16 @@
 package de.akra.coronatestmanagement.repository;
 
 import de.akra.coronatestmanagement.model.PersonGroup;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import javax.persistence.Entity;
 import java.time.LocalDate;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 public interface PersonGroupRepository extends JpaRepository<PersonGroup, UUID> {
-    Optional<PersonGroup> findById(UUID id);
 
-    //@Query("SELECT g.id AS groupId, COUNT(g.id) as numTests FROM PersonGroup g LEFT OUTER JOIN g.people p LEFT OUTER JOIN p.tests t WHERE t.date = :date GROUP BY g.id")
     @Query("""
               SELECT g AS group, COUNT(t.id) as numTests
               FROM PersonGroup g 
@@ -33,10 +28,4 @@ public interface PersonGroupRepository extends JpaRepository<PersonGroup, UUID> 
         PersonGroup getGroup();
         int getNumTests();
     }
-
-    /*public record GroupNumTests(PersonGroup group, int numTests) {
-        protected GroupNumTests() {
-            this(null, -1);
-        }
-    }*/
 }
